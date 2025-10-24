265e3649c958c9195fb2a756e8a12ed3e8181465
ecemoz <ecemnurozen03@gmail.com>
2025-10-24 12:21:23 +0300

    Initial Commit

---
+++ Habit_Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit_Tracker
+//
+//  Created by Ecem Nur Özen on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
